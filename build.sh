ccce9e85900d24df6bae4321f63f2963e44a6be3
Theodoro MARTIN AHUALLI <theodoro.ahualli-de-sanctis@insa-rennes.fr>
2023-02-08 17:23:50 -0500

    oops javac -d does not create destination directory

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ rm -f build/*.class
 find . -name \*.class -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac src/main/*.java -d ./build
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar src/test/*.java -d ./build -sourcepath ./src/main
+mkdir -p build
+javac src/main/*.java -d build
+javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar src/test/*.java -d build -sourcepath ./src/main
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 echo "Running unit tests..."
 java -cp ./build:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
